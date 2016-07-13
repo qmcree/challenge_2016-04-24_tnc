0d040206090e272abc19ff3fa6ff8d8166d2b8d3
Quentin McRee <qmcree@gmail.com>
2016-07-13 11:09:33 -0500

    Update ConverterTest.php
    Corrected terminology from "mock" to "fake".

--- tests/Currency/ConverterTest.php
+++ tests/Currency/ConverterTest.php
@@ -6,14 +6,14 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
 {
     public function testConvert()
     {
-        $converter = new MockConverter(new Dollar(10), new Euro());
+        $converter = new FakeConverter(new Dollar(10), new Euro());
         $actual = $converter->convert();
 
         $this->assertEquals($actual, 8);
     }
 }
 
-class MockConverter extends Converter
+class FakeConverter extends Converter
 {
     protected function rate()
     {
